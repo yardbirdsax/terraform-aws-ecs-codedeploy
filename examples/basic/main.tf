4295f302b1137dd33d2f74fab7151aa6165261fe
Josh Feierman <josh@sqljosh.com>
2021-05-14 17:23:59 -0400

    Addition of basic default VPC example
    An example deploying within the default VPC has been added.

---
+++ examples/basic/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">=0.12.31"
+  required_providers {
+    aws = {
+      version   = ">=3"
+      source    = "hashicorp/aws"
+    }
+  }
+}
+
+module "ecs_codedeploy" {
+  source = "../../"
+  container_image_name = "nginx-test"
+  container_image_tag  = "1.0"
+  deployment_name      = "awsecswithcodedeploybasic"
+}
